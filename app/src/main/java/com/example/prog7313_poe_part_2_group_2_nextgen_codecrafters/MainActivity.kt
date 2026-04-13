3a213ba9cca2b7aee43f2530bad129ca643bfd81
ST10445050 <ST10445050@vcconnect.edu.za>
2026-04-13 22:29:39 +0200

    Implemented navigation graph and connected screens in MainActivity

--- app/src/main/java/com/example/prog7313_poe_part_2_group_2_nextgen_codecrafters/MainActivity.kt
+++ app/src/main/java/com/example/prog7313_poe_part_2_group_2_nextgen_codecrafters/MainActivity.kt
@@ -3,45 +3,17 @@ package com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.navigation.AppNavGraph
 import com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.ui.theme.PROG7313POEPart2Group2NextGen_CodeCraftersTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             PROG7313POEPart2Group2NextGen_CodeCraftersTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavGraph()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PROG7313POEPart2Group2NextGen_CodeCraftersTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/prog7313_poe_part_2_group_2_nextgen_codecrafters/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/prog7313_poe_part_2_group_2_nextgen_codecrafters/navigation/AppNavGraph.kt
@@ -1,4 +1,44 @@
 package com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.navigation
 
-class AppNavGraph {
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.ui.auth.LandingScreen
+import com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.ui.auth.LoginScreen
+import com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.ui.auth.RegisterScreen
+
+@Composable
+fun AppNavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "landing"
+    ) {
+        composable("landing") {
+            LandingScreen(navController)
+        }
+
+        composable("login") {
+            LoginScreen(navController)
+        }
+
+        composable("register") {
+            RegisterScreen(navController)
+        }
+
+        composable("reset") {
+            Text("Reset Password Screen")
+        }
+
+        composable("question1") {
+            Text("Questionnaire Question 1")
+        }
+
+        composable("dashboard") {
+            Text("Dashboard Screen")
+        }
+    }
 }
