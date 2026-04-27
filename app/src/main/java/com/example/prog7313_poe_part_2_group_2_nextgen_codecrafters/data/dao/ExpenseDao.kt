591efe1f29e7b7ac3b1de7d9de5979a477f277cf
Teah <166519031+St10440926@users.noreply.github.com>
2026-04-27 18:31:04 +0200

    Added category totals query feature

--- app/src/main/java/com/example/prog7313_poe_part_2_group_2_nextgen_codecrafters/data/dao/ExpenseDao.kt
+++ app/src/main/java/com/example/prog7313_poe_part_2_group_2_nextgen_codecrafters/data/dao/ExpenseDao.kt
@@ -1,13 +1,32 @@
 package com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.data.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 import com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.data.entities.Expense
+import com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.model.CategoryTotal
 
 @Dao
 interface ExpenseDao {
+
+
     @Insert
     suspend fun insertExpense(expense: Expense)
 
     @Query("SELECT * FROM expenses WHERE userId = :userId")
     suspend fun getExpensesForUser(userId: Int): List<Expense>
+
+    //  added feature: Category Totals Report
+    @Query("""
+        SELECT categoryId, SUM(amount) as totalAmount
+        FROM expenses
+        WHERE userId = :userId
+        AND date BETWEEN :startDate AND :endDate
+        GROUP BY categoryId
+    """)
+    suspend fun getTotalSpentByCategory(
+        userId: Int,
+        startDate: String,
+        endDate: String
+    ): List<CategoryTotal>
 }
