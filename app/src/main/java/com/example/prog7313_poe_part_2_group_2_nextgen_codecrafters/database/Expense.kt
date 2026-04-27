f8ab5daf3483c10aca3d7b316712551bf8528567
ST10250993 <126852812+ST10250993@users.noreply.github.com>
2026-04-27 14:01:54 +0200

    feat: setup required data package structure

--- app/src/main/java/com/example/prog7313_poe_part_2_group_2_nextgen_codecrafters/database/Expense.kt
+++ app/src/main/java/com/example/prog7313_poe_part_2_group_2_nextgen_codecrafters/database/Expense.kt
@@ -1,17 +1,15 @@
-package com.example.prog7313_poe_part_2_group_2_nextgen_codecrafters.database
-
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "expenses")
 data class Expense(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
-    val amount: Double,
-    val category: String,
+    @PrimaryKey(autoGenerate = true) val expenseId: Int = 0,
+    val userId: Int,
+    val categoryId: Int,
     val date: String,
     val startTime: String,
     val endTime: String,
     val description: String,
-    val imageUri: String? = null
+    val amount: Double,
+    val photoPath: String? // Nullable [cite: 115]
 )
